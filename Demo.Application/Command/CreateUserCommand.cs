d769b4ecd10d1ac03830e7d13cac93bd1ab5d0a1
unknown <toandoan2804@gmail.com>
2025-07-01 21:46:18 +0700

    finish

--- Demo.Application/Command/CreateUserCommand.cs
+++ Demo.Application/Command/CreateUserCommand.cs
@@ -3,6 +3,8 @@ using Demo.Core.Response;
 using MediatR;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +13,13 @@ namespace Demo.Application.Command
 {
     public class CreateUserCommand : IRequest<BaseResponse<User>>
     {
+        [NotNull]
         public string Name { get; set; }
+        [EmailAddress(ErrorMessage = "Email is not valid")]
         public string Email { get; set; }
+        [NotNull]
         public string Password { get; set; }
+        [Phone(ErrorMessage = "Phone is not valid")]
         public string PhoneNumber { get; set; }
     }
 }
